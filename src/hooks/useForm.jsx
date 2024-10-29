88332e60ce2639c8cc857e0190933bbc095fb0e4
rulero98 <rortegaa1601@alumno.ipn.mx>
2024-10-28 21:44:41 -0600

    added thunk and provider to create a new user

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -10,7 +10,6 @@ export const useForm = ( initialForm = {} ) => {
 
   const onInputChange = ({ target }) => {
     const { value, name } = target
-    console.log(value,name)
     setFormState({
       ...formState,
       [name]: value
